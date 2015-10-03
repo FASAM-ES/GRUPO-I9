355b0f2fdfa9fb869d9a124b6db355769c7951c3
Alex Silva <Alex Silva@172.16.7.151>
2015-10-03 14:53:21 -0300

    Refatoração do teste retangulo e adicionado teste de negação

--- FigurasGeometricas/src/test/java/br/edu/fasam/figurasgeometricas/RetangulotTest.java
+++ FigurasGeometricas/src/test/java/br/edu/fasam/figurasgeometricas/RetangulotTest.java
@@ -1,51 +1,38 @@
 package br.edu.fasam.figurasgeometricas;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import junit.framework.TestCase;
 
-/**
- *
- * @author Alex Silva
- */
 public class RetangulotTest extends TestCase {
-    
+
+    private Retangulo retangulo;
+
     public RetangulotTest(String testName) {
         super(testName);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        retangulo = new Retangulo(15, 5);
     }
-    
+
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
-    
-    public void testCalcularArea()
-    {
-        Retangulo retangulo = new Retangulo(15,5);
-        assertEquals(75 , retangulo.getArea());
-    }
-    
-    public void testCalcularPerimetro()
-    {
-        Retangulo retangulo = new Retangulo(15,5);
-        assertEquals(40 , retangulo.getPerimetro());
-    }    
-    
-    public void testCalcularDiagonal()
-    {
-        Retangulo retangulo = new Retangulo(15,5);
-        assertEquals(15 , retangulo.getDiagonal());
-    }       
 
-    // TODO add test methods here. The name must begin with 'test'. For example:
-    // public void testHello() {}
+    public void testCalcularArea() {
+        assertEquals(75, retangulo.getArea());
+        assertFalse("Calculo incorreto", retangulo.getArea() == 76);
+    }
+
+    public void testCalcularPerimetro() {
+        assertEquals(40, retangulo.getPerimetro());
+        assertFalse("Calculo incorreto", retangulo.getPerimetro() == 41);
+    }
+
+    public void testCalcularDiagonal() {
+        assertEquals(15, retangulo.getDiagonal());
+        assertFalse("Calculo incorreto", retangulo.getDiagonal() == 16);
+    }
 }
