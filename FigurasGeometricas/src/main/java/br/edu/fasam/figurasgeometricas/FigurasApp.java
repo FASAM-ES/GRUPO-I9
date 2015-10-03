c280c89d374fed1c6d794faa3f239f8cac1195d7
sidine <sidine@172.16.6.30>
2015-10-03 16:56:30 -0300

    Teste de quadrado

--- FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
+++ FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
@@ -17,20 +17,20 @@ public class FigurasApp {
         System.out.println("Perímetro do quadrado: " + quadrado.calcularPerimetro());
    
         Retangulo retangulo = new Retangulo(5, 15);
-        System.out.printf("Retangulo - Base %.2f Altura %.2f\n", retangulo.getBase(), retangulo.getAltura());        
-        System.out.printf("Area = %d\n", retangulo.getArea());
-        System.out.printf("Perimetro = %d\n", retangulo.getPerimetro());
-        System.out.printf("Diagonal  = %d\n ", retangulo.getDiagonal());
+        System.out.println(String.format("Retangulo - Base %.2f Altura %.2f", retangulo.getBase(), retangulo.getAltura()));        
+        System.out.println(String.format("Area = %d", retangulo.getArea()));
+        System.out.println(String.format("Perimetro = %d", retangulo.getPerimetro()));
+        System.out.println(String.format("Diagonal  = %d ", retangulo.getDiagonal()));
         
         Circulo circulo = new Circulo(2.0);
         System.out.println("Calculo da área do circulo: " + circulo.calculaArea());
         System.out.println("Calculo do perímetro: " + circulo.calculaPerimentro());
         
         Triangulo triangulo = new Triangulo(10, 10, 15);
-        System.out.printf("Triangulo - Lado A: %d Lado B: %d Lado C: %d \n",
-        triangulo.getLadoA(), triangulo.getLadoB(),triangulo.getLadoC());        
-        System.out.printf("Area = %d\n", triangulo.calculeArea());
-        System.out.printf("Perimetro = %d\n", triangulo.calculePerimetro());
+        System.out.println(String.format("Triangulo - Lado A: %d Lado B: %d Lado C: %d ",
+        triangulo.getLadoA(), triangulo.getLadoB(),triangulo.getLadoC()));        
+        System.out.println(String.format("Area = %d", triangulo.calculeArea()));
+        System.out.println(String.format("Perimetro = %d", triangulo.calculePerimetro()));
         
     }
 }
