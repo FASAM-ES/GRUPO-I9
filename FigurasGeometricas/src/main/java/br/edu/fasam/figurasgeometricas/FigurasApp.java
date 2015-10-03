44ef7e7105a91623b4453520cd8e8fd1574bcd69
Alex Silva <Alex Silva@172.16.7.151>
2015-10-03 17:11:10 -0300

    Ajuste na impressão

--- FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
+++ FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
@@ -13,24 +13,28 @@ public class FigurasApp {
     public static void main(String[] args) {
         
         Quadrado quadrado = new Quadrado(25);
-        System.out.println("Área do quadrado: " + quadrado.calcularArea());
-        System.out.println("Perímetro do quadrado: " + quadrado.calcularPerimetro());
+        Imprimir("Área do quadrado: " + quadrado.calcularArea());
+        Imprimir("Perímetro do quadrado: " + quadrado.calcularPerimetro());
    
         Retangulo retangulo = new Retangulo(5, 15);
-        System.out.println(String.format("Retangulo - Base %.2f Altura %.2f", retangulo.getBase(), retangulo.getAltura()));        
-        System.out.println(String.format("Area = %d", retangulo.getArea()));
-        System.out.println(String.format("Perimetro = %d", retangulo.getPerimetro()));
-        System.out.println(String.format("Diagonal  = %d ", retangulo.getDiagonal()));
+        Imprimir(String.format("Retangulo - Base %.2f Altura %.2f", retangulo.getBase(), retangulo.getAltura()));        
+        Imprimir(String.format("Area = %d", retangulo.getArea()));
+        Imprimir(String.format("Perimetro = %d", retangulo.getPerimetro()));
+        Imprimir(String.format("Diagonal  = %d ", retangulo.getDiagonal()));
         
         Circulo circulo = new Circulo(2.0);
-        System.out.println("Calculo da área do circulo: " + circulo.calculaArea());
-        System.out.println("Calculo do perímetro: " + circulo.calculaPerimentro());
+        Imprimir("Calculo da área do circulo: " + circulo.calculaArea());
+        Imprimir("Calculo do perímetro: " + circulo.calculaPerimentro());
         
         Triangulo triangulo = new Triangulo(10, 10, 15);
-        System.out.println(String.format("Triangulo - Lado A: %d Lado B: %d Lado C: %d ",
+        Imprimir(String.format("Triangulo - Lado A: %d Lado B: %d Lado C: %d ", 
         triangulo.getLadoA(), triangulo.getLadoB(),triangulo.getLadoC()));        
-        System.out.println(String.format("Area = %d", triangulo.calculeArea()));
-        System.out.println(String.format("Perimetro = %d", triangulo.calculePerimetro()));
-        
+        Imprimir(String.format("Area = %d", triangulo.calculeArea()));
+        Imprimir(String.format("Perimetro = %d", triangulo.calculePerimetro()));        
+    }
+    
+    private static void Imprimir(String texto)
+    {
+        System.out.println(texto);
     }
 }
