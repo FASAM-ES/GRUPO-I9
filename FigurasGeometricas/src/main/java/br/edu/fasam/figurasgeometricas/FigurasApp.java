210a5bc1e5be099b4d04442b57f7949f271b569f
Alex Silva <Alex Silva@172.16.7.151>
2015-10-03 11:23:38 -0300

    Formatação

--- FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
+++ FigurasGeometricas/src/main/java/br/edu/fasam/figurasgeometricas/FigurasApp.java
@@ -1,10 +1,10 @@
 package br.edu.fasam.figurasgeometricas;
-        
+
 public class FigurasApp {
+
     public static void main(String[] args) {
-        for(int index = 0; index < 10;index++)
-        {
-        System.out.println("Olá Mundo!");
+        for (int index = 0; index < 10; index++) {
+            System.out.println("Olá Mundo!");
         }
     }
 }
